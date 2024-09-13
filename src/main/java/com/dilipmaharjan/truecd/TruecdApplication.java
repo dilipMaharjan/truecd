57331ae7a5a10e1ca0d914c3211c4e90671b2ff9
dilipMaharjan <dilipmaharjann@gmail.com>
2024-09-13 09:39:35 -0500

    Initial setup

---
+++ src/main/java/com/dilipmaharjan/truecd/TruecdApplication.java
@@ -0,0 +1,13 @@
+package com.dilipmaharjan.truecd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TruecdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TruecdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dilipmaharjan/truecd/TruecdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dilipmaharjan.truecd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TruecdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
