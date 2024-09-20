d12e436eaf00f30abcae7439a6e602fd21dffe2e
dilipMaharjan <dilipmaharjann@gmail.com>
2024-09-20 11:42:09 -0500

    Some configurations.

--- src/test/java/karate-config.js
+++ src/test/java/karate-config.js
@@ -1,1 +1,18 @@
-function fn(){ return {} }
+function fn(){
+    var env=karate.env; //get system property ' karate.env'
+    karate.log('karate.env system property was:' env);
+    if(!env){
+        env='dev';
+    }
+    var config={
+    env: env,
+        myVarName:'SomeValue'
+    }
+
+    if(env=='dev'){
+        //customize
+    }else if(env=='e2e'){
+        //customize
+    }
+    return config;
+}
